dc5e7c338d78bd838213fb799869496fe3cd56dd
efoejean <efoejean@gmail.com>
2023-01-05 21:45:16 -0600

    using createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Appointment from "./routes/Appointment";
-import Scheduler from "./routes/Scheduler";
-
-import Navbar from "./components/Navbar";
+import Root from "./routes/Root";
 
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+
+    children: [
+      {
+        path: "/appointment",
+        element: <Appointment />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/agenda" element={<Scheduler />} />
-        <Route path="/appointment" element={<Appointment />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
+
 export default App;
