860853918cd2f0d3fb14223d2b94a531f128e6f2
efoejean <efoejean@gmail.com>
2023-01-19 22:06:09 -0600

    adding edit , cancel and complete button to the appointment component

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -1,48 +1,84 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import useAppointment from "../hooks/useAppointment";
 import { getNavIds } from "../utils/pagination";
+import AppointmentModal from "./AppointmentModal";
 
 export default function Appoint() {
+  const [isOpen, setIsOpen] = useState(false);
   const { appointment, ids } = useAppointment();
   const { prevId, nextId } = getNavIds(ids, appointment.id);
+
   return (
-    <figure className="container mx-auto flex flex-col gap-y-4 rounded-md border bg-sky-700 px-8 py-8 text-zinc-50 shadow-xl">
+    <>
       <div>
-        <div className="flex items-center gap-x-4">
-          <figcaption>
-            <h2 className="text-2xl font-bold text-black">
-              {appointment.clientName}
-            </h2>
-            <small>{appointment.service}</small>
-            <blockquote className="italic">
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-              }).format(appointment.price)}
-            </blockquote>
-          </figcaption>
+        <div className="mb-10 flex justify-center">
+          <Link to="/">
+            <button color="blue">Back to Appointments</button>
+          </Link>
         </div>
-        <div className="flex items-center gap-x-4">
-          <figcaption>
-            <h2 className="text-2xl font-bold text-black">
-              {new Date(appointment.appointment_date).toLocaleDateString()}
-            </h2>
-            <small>{appointment.clientPhoneNumber}</small>
-            <blockquote className="italic">
-              &quot;{appointment.status}&quot;
-            </blockquote>
-          </figcaption>
+        <div className="... flex flex-row justify-center gap-10">
+          <div className="">
+            <button
+              className="editBtn mr-12"
+              type="submit"
+              onClick={() => setIsOpen(true)}
+            >
+              Edit
+            </button>
+            {isOpen && <AppointmentModal setIsOpen={setIsOpen} />}
+          </div>
+          <div className="">
+            <button className="cancelBtn mr-12" type="submit">
+              Cancel
+            </button>
+          </div>
+          <div className="">
+            <button className="primaryBtn mr-12" type="submit">
+              Complete
+            </button>
+          </div>
         </div>
       </div>
 
-      <footer className="self-center text-xl">
-        <Link to={"/" + prevId} className="white mr-4">
-          ⬅️ Prev
-        </Link>
-        <Link to={"/" + nextId} className="white">
-          Next ➡️
-        </Link>
-      </footer>
-    </figure>
+      <figure className="container mx-auto flex flex-col gap-y-4 rounded-md border bg-sky-700 px-8 py-8 text-zinc-50 shadow-xl">
+        <div>
+          <div className="flex items-center gap-x-4">
+            <figcaption>
+              <h2 className="text-2xl font-bold text-black">
+                {appointment.clientName}
+              </h2>
+              <small>{appointment.service}</small>
+              <blockquote className="italic">
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(appointment.price)}
+              </blockquote>
+            </figcaption>
+          </div>
+          <div className="flex items-center gap-x-4">
+            <figcaption>
+              <h2 className="text-2xl font-bold text-black">
+                {new Date(appointment.appointment_date).toLocaleDateString()}
+              </h2>
+              <small>{appointment.clientPhoneNumber}</small>
+              <blockquote className="italic">
+                &quot;{appointment.status}&quot;
+              </blockquote>
+            </figcaption>
+          </div>
+        </div>
+
+        <footer className="self-center text-xl">
+          <Link to={"/" + prevId} className="white mr-4">
+            ⬅️ Prev
+          </Link>
+          <Link to={"/" + nextId} className="white">
+            Next ➡️
+          </Link>
+        </footer>
+      </figure>
+    </>
   );
 }
