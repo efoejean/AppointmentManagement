e8f933868d1ef740efc1a22059d55512db90d4b3
efoejean <efoejean@gmail.com>
2023-01-31 19:47:05 -0600

    Clean up

--- src/App.jsx
+++ src/App.jsx
@@ -3,106 +3,57 @@ import {
   redirect,
   RouterProvider,
 } from "react-router-dom";
-import Appoint from "./components/Appointment";
-import Table from "./components/Table/Table";
-import Cancel from "./routes/Cancel";
-import Delete from "./routes/Delete";
 import Root from "./routes/Root";
-import Scheduler from "./routes/Scheduler";
-import {
-  createAppointment,
-  deleteOneAppointment,
-  getAppointments,
-  updateOneAppointment,
-} from "./services/axios";
 
 import "./index.css";
 
-const createEditAppointment = async ({ request }) => {
-  const fd = await request.formData();
-  const createdEditedAppointment = Object.fromEntries(fd.entries());
+// const createEditAppointment = async ({ request }) => {
+//   const fd = await request.formData();
+//   const createdEditedAppointment = Object.fromEntries(fd.entries());
 
-  try {
-    const { id } =
-      // 'id' may or may not be defined depending on whether we are creating or updating
-      createdEditedAppointment.id
-        ? await updateOneAppointment(createdEditedAppointment.id, {
-            appointment_date: createdEditedAppointment.appointment_date,
-            clientName: createdEditedAppointment.clientName,
-            clientPhoneNumber: createdEditedAppointment.clientPhoneNumber,
-            stylistName: createdEditedAppointment.stylistName,
-            service: createdEditedAppointment.service,
-            deposit: createdEditedAppointment.deposit,
-            price: createdEditedAppointment.price,
-          })
-        : await createAppointment(createdEditedAppointment);
+//   try {
+//     const { id } =
+//       // 'id' may or may not be defined depending on whether we are creating or updating
+//       createdEditedAppointment.id
+//         ? await updateOneAppointment(createdEditedAppointment.id, {
+//             appointment_date: createdEditedAppointment.appointment_date,
+//             clientName: createdEditedAppointment.clientName,
+//             clientPhoneNumber: createdEditedAppointment.clientPhoneNumber,
+//             stylistName: createdEditedAppointment.stylistName,
+//             service: createdEditedAppointment.service,
+//             deposit: createdEditedAppointment.deposit,
+//             price: createdEditedAppointment.price,
+//           })
+//         : await createAppointment(createdEditedAppointment);
 
-    // Must return a redirect action
-    return redirect(`/appointment/${id}`);
-  } catch (error) {
-    // TODO: redirect to error page
-    console.error(error);
-  }
-};
+//     // Must return a redirect action
+//     return redirect(`/appointment/${id}`);
+//   } catch (error) {
+//     // TODO: redirect to error page
+//     console.error(error);
+//   }
+// };
 
-const loadUsersAppointments = async () => {
-  const Appointments = await getAppointments();
+// const loadUsersAppointments = async () => {
+//   const Appointments = await getAppointments();
 
-  const AppointmentsData = Appointments.map((appointment) => ({
-    ...appointment,
+//   const AppointmentsData = Appointments.map((appointment) => ({
+//     ...appointment,
 
-    start: new Date(appointment.appointment_date),
-    end: new Date(appointment.appointment_date),
+//     start: new Date(appointment.appointment_date),
+//     end: new Date(appointment.appointment_date),
 
-    stylist: appointment.stylistName,
-    title: appointment.clientName + " / " + appointment.service,
-  }));
+//     stylist: appointment.stylistName,
+//     title: appointment.clientName + " / " + appointment.service,
+//   }));
 
-  return { AppointmentsData };
-};
+//   return { AppointmentsData };
+// };
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    loader: loadUsersAppointments,
-    action: createEditAppointment,
-    children: [
-      {
-        path: "",
-        element: <Table />,
-      },
-      {
-        path: "/appointment/:id",
-
-        element: <Appoint />,
-        loader: loadUsersAppointments,
-        action: createEditAppointment,
-      },
-      {
-        path: "/agenda",
-        element: <Scheduler />,
-      },
-      {
-        path: "Cancel/:id",
-        element: <Cancel />,
-        action: async ({ params }) => {
-          await updateOneAppointment(params.id, {
-            status: "cancelled",
-          });
-          return redirect("/");
-        },
-      },
-      {
-        path: "Delete/:id",
-        element: <Delete />,
-        action: async ({ params }) => {
-          console.log(params.id);
-          await deleteOneAppointment(params.id);
-          return redirect("/");
-        },
-      },
-    ],
   },
   {
     path: "*",
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,40 +1,50 @@
-import { Outlet, useLoaderData } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
+// export async function loader() {
+//   const Appointments = await getAppointments();
+
+//   const AppointmentsData = Appointments.map((appointment) => ({
+//     ...appointment,
+
+//     start: new Date(appointment.appointment_date),
+//     end: new Date(appointment.appointment_date),
+
+//     stylist: appointment.stylistName,
+//     title: appointment.clientName + " / " + appointment.service,
+//   }));
+
+//   return { AppointmentsData };
+// }
 export default function Root() {
   // useLoaderData() is a hook that returns the data - be sure to DESTRUCTURE it!
-  const { AppointmentsData } = useLoaderData();
+  // const { AppointmentsData } = useLoaderData();
 
-  // TODO: Refactor this to avoid as much clutter. Consider spread operator.
-  const tableData = AppointmentsData.map(
-    ({
-      id,
-      appointment_date,
-      clientName,
-      clientPhoneNumber,
-      deposit,
-      service,
-      price,
-      status,
-    }) => ({
-      id,
-      appointment_date: new Date(appointment_date).toLocaleDateString(),
-      clientName,
-      clientPhoneNumber,
-      deposit,
-      service,
-      price: `$${price}`,
-      status,
-    })
-  );
+  // // TODO: Refactor this to avoid as much clutter. Consider spread operator.
+  // const tableData = AppointmentsData.map(
+  //   ({
+  //     id,
+  //     appointment_date,
+  //     clientName,
+  //     clientPhoneNumber,
+  //     deposit,
+  //     service,
+  //     price,
+  //     status,
+  //   }) => ({
+  //     id,
+  //     appointment_date: new Date(appointment_date).toLocaleDateString(),
+  //     clientName,
+  //     clientPhoneNumber,
+  //     deposit,
+  //     service,
+  //     price: `$${price}`,
+  //     status,
+  //   })
+  // );
 
   return (
     <>
       <Navbar />
-      <main className="text-3ml mx-8 mt-4 mb-8 flex flex-col gap-y-4 text-center font-bold">
-        {/* TODO: Is 'data' actually used or do we just need tableDAta? */}
-        <Outlet context={{ data: AppointmentsData, tableData }} />
-      </main>
     </>
   );
 }
